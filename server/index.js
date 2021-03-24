9eec12d6cfc1cf9f08ececedacf3b9fd432508ca
Ryan Garren <rryangarren@gmail.com>
2021-03-24 17:07:52 -0600

    up to step 2.part 1.line 4

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
 const massive = require('massive')
+const authCtrl = require('./controllers/authController')
 
 const PORT = 4000
 
@@ -11,19 +12,21 @@ const app = express()
 
 app.use(express.json())
 
-massive({
-  connectionString: CONNECTION_STRING,
-  ssl: { rejectUnauthorized: false }
-}).then(db => {
-  app.set('db', db)
-  app.listen(PORT, () => console.log(`DB connected and server listening on port ${PORT}`))
-})
-.catch(err => console.log(err))
-
 app.use(
   session({
     resave: true,
     saveUninitialized: false,
     secret: SESSION_SECRET
   })
-)
+  )
+  
+  massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false }
+  }).then(db => {
+    app.set('db', db)
+    app.listen(PORT, () => console.log(`DB connected and server listening on port ${PORT}`))
+  })
+  .catch(err => console.log(err))
+
+  
